c8e63d533459863de2ffbb8d8b05a1f450b94354
Nuss Maciej <informatyk@nuss.pl>
2016-03-09 19:35:32 +0100

    Zapis

--- Uniszofer/Konfiguracja.Designer.cs
+++ Uniszofer/Konfiguracja.Designer.cs
@@ -28,20 +28,51 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.barPostep = new System.Windows.Forms.ProgressBar();
+            this.txtPostep = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // barPostep
+            // 
+            this.barPostep.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.barPostep.Location = new System.Drawing.Point(0, 166);
+            this.barPostep.Maximum = 10;
+            this.barPostep.Name = "barPostep";
+            this.barPostep.Size = new System.Drawing.Size(369, 23);
+            this.barPostep.TabIndex = 0;
+            // 
+            // txtPostep
+            // 
+            this.txtPostep.BackColor = System.Drawing.Color.White;
+            this.txtPostep.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtPostep.Location = new System.Drawing.Point(0, 0);
+            this.txtPostep.Multiline = true;
+            this.txtPostep.Name = "txtPostep";
+            this.txtPostep.ReadOnly = true;
+            this.txtPostep.Size = new System.Drawing.Size(369, 166);
+            this.txtPostep.TabIndex = 1;
             // 
             // Konfiguracja
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(383, 261);
+            this.ClientSize = new System.Drawing.Size(369, 189);
+            this.Controls.Add(this.txtPostep);
+            this.Controls.Add(this.barPostep);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Konfiguracja";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Uniszofer";
+            this.Load += new System.EventHandler(this.Konfiguracja_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ProgressBar barPostep;
+        private System.Windows.Forms.TextBox txtPostep;
     }
 }
 
--- Uniszofer/Konfiguracja.cs
+++ Uniszofer/Konfiguracja.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace Uniszofer
 {
@@ -14,7 +16,59 @@ namespace Uniszofer
     {
         public Konfiguracja()
         {
-            InitializeComponent();
+            InitializeComponent();            
+        }
+
+        private void Konfiguracja_Load(object sender, EventArgs e)
+        {
+           
+            txtPostep.Text = txtPostep.Text + "[" + DateTime.Now + "] - Uruchomienie programu \r\n";
+            barPostep.Value = barPostep.Value + 1;
+
+            try
+            {
+                txtPostep.Text = txtPostep.Text + "[" + DateTime.Now + "] - Wyszukiwanie pliku konfiguracyjnego \r\n";
+                 barPostep.Value = barPostep.Value + 1;
+                if (File.Exists(@"config.txt"))
+                {
+                    txtPostep.Text = txtPostep.Text + "[" + DateTime.Now + "] - Znaleziono plik konfiguracyjny \r\n";
+                    barPostep.Value = barPostep.Value + 1;
+
+                    txtPostep.Text = txtPostep.Text + "[" + DateTime.Now + "] - Otwieram konfigurację \r\n";
+                    barPostep.Value = barPostep.Value + 1;
+
+                    string[] lines = System.IO.File.ReadAllLines(@"config.txt");
+                       String data_instalacji = lines[0];
+                       String hash = lines[1];
+                       String host = lines[2];
+                       String port = lines[3];
+                       String login = lines[4];
+                       String haslo = lines[5];
+                       String baza = lines[6];
+
+                    txtPostep.Text = txtPostep.Text + "[" + DateTime.Now + "] - Konfiguracja odczytana \r\n";
+                    barPostep.Value = barPostep.Value + 1;
+
+                    txtPostep.Text = txtPostep.Text + "[" + DateTime.Now + "] - Dekompresja konfiguracji \r\n";
+                    barPostep.Value = barPostep.Value + 1;
+                    
+                    
+
+
+                }
+                else
+                {
+                    txtPostep.Text = txtPostep.Text + "[" + DateTime.Now + "] - Brak pliku konfiguracyjnego !";
+                    barPostep.Value = barPostep.Value + 1;
+                    
+                }
+
+            }
+            catch (Exception komunikat)
+            {
+                MessageBox.Show(komunikat.Message);
+            }
+
         }
     }
 }
