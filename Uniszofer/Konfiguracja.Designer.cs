387d0036def51c7e478b96ad1d0bea5559c5ab22
Nuss Maciej <informatyk@nuss.pl>
2016-03-09 18:26:11 +0100

    Forma Konfiguracja

---
+++ Uniszofer/Konfiguracja.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace Uniszofer
+{
+    partial class Konfiguracja
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Wymagana metoda obsługi projektanta — nie należy modyfikować 
+        /// zawartość tej metody z edytorem kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Konfiguracja
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(383, 261);
+            this.Name = "Konfiguracja";
+            this.Text = "Uniszofer";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
